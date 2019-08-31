b2b0355c16bb8808d87a8b857d25a066d68d0e3a
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-08-31 20:55:31 +0200

    fixed week

--- src/app/ausbildungsnachweis/ausbildungsnachweis.component.ts
+++ src/app/ausbildungsnachweis/ausbildungsnachweis.component.ts
@@ -22,13 +22,31 @@ export class AusbildungsnachweisComponent implements OnInit {
     private service: AusbildungsnachweisService,
     private user: AuthService,
     public afs: AngularFirestore,
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.nachweisNummer = +this.route.snapshot.paramMap.get('nummer');
     this.nachweisWoche = this.service.loadNachweis(this.nachweisNummer);
     this.benutzer = JSON.parse(localStorage.getItem('user'));
     this.benutzerDaten = this.afs.doc(`users/${this.benutzer.uid}`).valueChanges();
+    console.log(this.getDateByWeek(35, 2019));
   }
-
+  getDate(weekIso, year) {
+    console.log(weekIso, year);
+    const d = this.getDateByWeek(weekIso, year);
+    const montag = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+    const freitag = new Date(d.getFullYear(), d.getMonth(), (d.getDate() + 4));
+    return montag.getDate() + '.' + montag.getMonth() + '.' + montag.getFullYear() +
+      ' - ' + freitag.getDate() + '.' + freitag.getMonth() + '.' + freitag.getFullYear();
+  }
+  getDateByWeek(weeks, year) {
+    const d = new Date(year, 0, 1);
+    const dayNum = d.getDay();
+    let requiredDate = (weeks - 2) * 7;
+    if (((dayNum != 0) || dayNum > 4)) {
+      requiredDate += 7;
+    }
+    d.setDate(1 - d.getDay() + ++requiredDate);
+    return d;
+  }
 }
