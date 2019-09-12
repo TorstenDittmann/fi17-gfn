8954b369de14e7c74742684412ed2036a2bae409
Torsten Dittmann <torsten.dittmann@googlemail.com>
2019-09-12 22:46:17 +0200

    update Web Worker

--- src/app/startseite/startseite.component.ts
+++ src/app/startseite/startseite.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { version } from '../../../package.json';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-startseite',
@@ -13,13 +14,17 @@ export class StartseiteComponent implements OnInit {
   benutzerDaten: Observable<any>;
   benutzer: any;
   benutzerDoc: AngularFirestoreDocument<any>;
+  neueVersion = false;
 
-  constructor(public afs: AngularFirestore, ) { }
+  constructor(public afs: AngularFirestore, private swUpdate: SwUpdate) { }
 
   ngOnInit() {
     this.benutzer = JSON.parse(localStorage.getItem('user'));
     this.benutzerDoc = this.afs.doc(`users/${this.benutzer.uid}`);
     this.benutzerDaten = this.benutzerDoc.valueChanges();
+    this.swUpdate.available.subscribe(evt => {
+      this.neueVersion = true;
+    });
   }
 
   editDisplayName(value) {
